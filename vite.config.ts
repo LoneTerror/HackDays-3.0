628e8faed317dee61341fc43a2d9e8beb4d6e63f
LoneTerror <nothingshere21@gmail.com>
2025-12-12 20:33:16 +0530

    Regular Maintenance - Dependencies Updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["powersense.site"],
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
